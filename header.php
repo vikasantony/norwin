03189967e4b4fb71a048e7752cc2fd073225568f
vikasantony <81165402+vikasantony@users.noreply.github.com>
2022-02-16 15:24:51 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,124 @@
+<?php
+echo' <header id="rs-header" class="rs-header">
+                <div class="menu-area menu-sticky">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="logo-area">
+                                    <a href="#"><img src="assets/images/logo-dark.png" alt="logo"></a>
+                                </div>
+                            </div>
+                            <div class="col-lg-8 text-right">
+                                <div class="rs-menu-area">
+                                    <div class="main-menu">
+                                        <div class="mobile-menu">
+                                            <a class="rs-menu-toggle">
+                                                <i class="fa fa-bars"></i>
+                                            </a>
+                                        </div>
+                                        <nav class="rs-menu">
+                                            <ul class="nav-menu">
+												
+                                                <li class="rs-mega-menu mega-rs current-menu-item"> <a href="#">Who we are</a>
+                                                   
+                                                </li>
+												
+
+                                                <li class="menu-item-has-children">
+                                                    <a href="#">What we do</a>
+												 <ul class="sub-menu">
+                                                        <li><a href="#">Practices</a></li>
+                                                        <li><a href="#">Delivery Models</a></li></ul>
+                                                </li>
+
+                                                <li class="menu-item-has-children">
+                                                    <a href="#">How we do it</a>
+                                                    <ul class="sub-menu">
+                                                       
+														 <li><a href="#">NorStaff</a></li>
+                                                        <li><a href="#">Global Enablement Center</a></li>
+                                                        <li><a href="#">Team in a Box</a></li>
+                                                        <li><a href="#">Deploy Governance</a></li>
+                                                     
+                                                     
+                                                    </ul>
+                                                </li>
+
+                                          
+                                                <li class="">
+                                                    <a href="#">MyNorwin</a>
+                                             
+                                                </li>
+												
+												   <li class="">
+                                                    <a href="#">Careers</a>
+                                             
+                                                </li>
+
+                                            </ul> 
+                                        </nav>
+                                    </div> 
+                                    <div class="expand-btn-inner">
+                                        <ul>
+                                       
+                                            <li>
+                                                <a id="nav-expander" class="humburger nav-expander" href="#">
+                                                    <span class="dot1"></span>
+                                                    <span class="dot2"></span>
+                                                    <span class="dot3"></span>
+                                                    <span class="dot4"></span>
+                                                    <span class="dot5"></span>
+                                                    <span class="dot6"></span>
+                                                    <span class="dot7"></span>
+                                                    <span class="dot8"></span>
+                                                    <span class="dot9"></span>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
+
+                
+                <nav class="right_menu_togle hidden-md">
+                    <div class="close-btn">
+                        <span id="nav-close" class="humburger">
+                            <span class="dot1"></span>
+                            <span class="dot2"></span>
+                            <span class="dot3"></span>
+                            <span class="dot4"></span>
+                            <span class="dot5"></span>
+                            <span class="dot6"></span>
+                            <span class="dot7"></span>
+                            <span class="dot8"></span>
+                            <span class="dot9"></span>
+                        </span>
+                    </div>
+                    <div class="canvas-logo">
+                        <a href="#"></a>
+                    </div>
+                    <div class="offcanvas-text">
+                        <p>Consectetur adipiscing elit. Duis at dictum risus, non suscip it arcu. Quisque aliquam posuere tortor aliquam posuere tortor develop database.</p>
+                    </div>
+                    <div class="canvas-contact">
+                        <ul class="contact">
+                            <li><i class="flaticon-location"></i> 374 William S Canning Blvd, Fall River MA 2721, USA</li>
+                            <li><i class="flaticon-call"></i><a href="tel:+880155-69569">(+880)155-69569</a></li>
+                            <li><i class="flaticon-email"></i><a href="mailto:support@rstheme.com">support@rstheme.com</a></li>
+                            <li><i class="flaticon-clock"></i>10:00 - 17:00</li>
+                        </ul>
+                        <ul class="social">
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </nav>
+              
+            </header>';
+?>
